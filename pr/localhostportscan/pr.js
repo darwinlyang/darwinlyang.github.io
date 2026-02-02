e8792df7458e05be89f3bbed21e6d1996aa0a2f0
Darwin Yang <darwinyang@chromium.org>
2026-02-02 14:48:09 -0500

    Add a localhost port scan page.

---
+++ pr/localhostportscan/pr.js
@@ -0,0 +1,55 @@
+/* global done:false */
+/* global error:false */
+/* global PaymentRequest:false */
+
+/**
+ * Launches payment request for credit cards.
+ */
+async function onScanClicked() {  // eslint-disable-line no-unused-vars
+  let port = document.getElementById("port").value;
+  info(`Scanning port: ${port}`)
+
+  if (!window.PaymentRequest) {
+    error('PaymentRequest API is not supported.');
+    return;
+  }
+
+  var spinner = document.createElement('i');
+  spinner.classList = 'fa fa-refresh fa-spin';
+  var button = document.getElementById('scanButton');
+  button.appendChild(spinner);
+
+  try {
+    const request = new PaymentRequest(
+      [{ supportedMethods: `http://localhost:${port}/manifest.json` }],
+      {
+        total: {
+          label: "Total",
+          amount: { currency: "USD", value: "1.00" }
+        }
+      }
+    );
+    const response = await request.show();
+    info("Payment success: " + response);
+    await response.complete("success");
+  } catch (err) {
+    error("Payment failed: " + err);
+    let errMsg = err.message;
+    if (errMsg.includes('ERR_CONNECTION_REFUSED')) {
+      info(`Port ${port} result: ERR_CONNECTION_REFUSED`);
+    } else {
+      if (errMsg.indexOf('HTTP header') !== -1) {
+        info(`Port ${port} result: ${errMsg}`);
+      } else if (errMsg.indexOf('HTTP') !== -1) {
+        info(`Port ${port} result: ${errMsg.slice(errMsg.indexOf('HTTP'))}`);
+      } else if (errMsg.indexOf('ERR_') !== -1) {
+        info(`Port ${port} result: ${errMsg.slice(errMsg.indexOf('ERR_'))}`);
+      } else {
+        info(`Port ${port} result: ${errMsg}`);
+      }
+    }
+
+  } finally {
+    button.removeChild(spinner);
+  }
+}
